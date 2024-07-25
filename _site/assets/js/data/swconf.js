d77b4092855be7029698c11a3983f050965213e6
Ohsu Kwon <okwon39@gatech.edu>
2024-07-25 13:40:33 -0400

    add _site to .gitignore

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1721928962',resources: [
+    cacheName: 'chirpy-1721929134',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
