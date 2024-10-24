48606ecb77764f392f2d16825a0b81d4fd7ed718
Ohsu Kwon <okwon39@gatech.edu>
2024-10-24 14:53:14 -0400

    remove statement

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729794676',resources: [
+    cacheName: 'chirpy-1729795983',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
