345623846a0a222126f3730ab5139163968e32fe
Ohsu Kwon <okwon39@gatech.edu>
2024-10-24 14:06:41 -0400

    test

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729792305',resources: [
+    cacheName: 'chirpy-1729792522',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
